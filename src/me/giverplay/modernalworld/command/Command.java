d445f0207458c7e9051a287ef8de526f1fadbe00
GiverPlay007 <play.giver.007@gmail.com>
2020-05-18 20:41:30 -0300

    1.0.0 | Indentação AbstractCommand

--- src/me/giverplay/modernalworld/command/Command.java
+++ src/me/giverplay/modernalworld/command/Command.java
@@ -3,36 +3,36 @@ package me.giverplay.modernalworld.command;
 import org.bukkit.command.CommandSender;
 
 public abstract class Command
+{
+	private String name, usage;
+	private CommandType type;
+	
+	public Command(String name, String usage, CommandType type)
 	{
-		private String name, usage;
-		private CommandType type;
-		
-		public Command(String name, String usage, CommandType type)
-		{
-			this.name = name;
-			this.type = type;
-			this.usage = usage;
-		}
-		
-		public String getName()
-		{
-			return this.name;
-		}
-		
-		public String getUsage()
-		{
-			return "§cUtilize : " + this.usage;
-		}
-		
-		public CommandType getType()
-		{
-			return this.type;
-		}
-		
-		public String getPermission()
-		{
-			return "modernalworld.command." + this.name;
-		}
-		
-		public abstract void execute(CommandSender sender, String[] args);
+		this.name = name;
+		this.type = type;
+		this.usage = usage;
 	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public String getUsage()
+	{
+		return "§cUtilize : " + this.usage;
+	}
+	
+	public CommandType getType()
+	{
+		return this.type;
+	}
+	
+	public String getPermission()
+	{
+		return "modernalworld.command." + this.name;
+	}
+	
+	public abstract void execute(CommandSender sender, String[] args);
+}
